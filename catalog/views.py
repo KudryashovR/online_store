fc92b861b28abc65330fd7a257440314f256c56b
KudryashovR <romkud55@gmail.com>
2024-05-27 23:44:26 +0300

    ADD: app POST request

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+import datetime
+
 
 def home(request):
     return render(request, 'catalog/home.html')
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+
+        with open('messages.txt', 'a+', encoding='utf-8') as file:
+            file.write(f"{datetime.datetime.now()} - {name} ({phone}): {message}\n")
+
     return render(request, 'catalog/contacts.html')
