10e1486c6d05e449ea559e8d795cccc22b71ad3b
KudryashovR <romkud55@gmail.com>
2024-06-03 22:55:49 +0300

    ADD: Product and Category models fields

--- catalog/models.py
+++ catalog/models.py
@@ -1,19 +1,34 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
+
 
 class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование продукта')
+    description = models.TextField(verbose_name='Описание', help_text='Введите описание продукта', **NULLABLE)
+    preview = models.ImageField(upload_to='products/', verbose_name='Изображение',
+                                help_text='Загрузите изображение продукта', **NULLABLE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='products')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена за покупку',
+                                help_text='Введите цену продукта')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        pass
+        return self.name
 
     class Meta:
-        pass
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
 
 class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование категории')
+    description = models.TextField(verbose_name='Описание', help_text='Введите описание категории', **NULLABLE)
 
     def __str__(self):
-        pass
+        return self.name
 
     class Meta:
-        pass
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
