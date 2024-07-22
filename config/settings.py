665bf137ba8fb462db86685995fb3292a5771198
KudryashovR <romkud55@gmail.com>
2024-07-22 14:13:11 +0300

    REFACTOR: cache settings in env

--- config/settings.py
+++ config/settings.py
@@ -167,6 +167,6 @@ LOGOUT_REDIRECT_URL = '/'
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379'
+        'LOCATION': env('CACHES_LOCATION')
     }
 }
